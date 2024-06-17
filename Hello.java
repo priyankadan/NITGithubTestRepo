49f1529bdfa38498615422cbc00ef7a22d340b71
priyankadan <96962660+priyankadan@users.noreply.github.com>
2024-06-17 19:40:00 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         int x=10;
-        System.out.println(x+"hello file....");
+        System.out.println(x+"hello file...changed from github.");
     }
 
 }
